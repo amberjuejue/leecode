0b500d63922e2e476c46f992e08b25e952fda79c
wangyujue <amberjuejue@gmail.com>
2019-04-03 11:15:42 +0800

    Create EqualsTest.java

---
+++ src/main/java/com/wyj/leecode/string/EqualsTest.java
@@ -0,0 +1,24 @@
+package com.wyj.leecode.string;
+
+/**
+ * @author wangyujue
+ */
+public class EqualsTest {
+    public static void main(String[] args) {
+        String str1 = "HelloFlyapi";
+        String str2 = "HelloFlyapi";
+        String str3 = new String("HelloFlyapi");
+        String str4 = "Hello";
+        String str5 = "Flyapi";
+        String str6 = "Hello" + "Flyapi";
+        String str7 = str4 + str5;
+        System.out.println("str1 == str2 result: " + (str1 == str2));
+        System.out.println("str1 == str3 result: " + (str1 == str3));
+        System.out.println("str1 == str6 result: " + (str1 == str6));
+        System.out.println("str1 == str7 result: " + (str1 == str7));
+        System.out.println("str1 == str7.intern() result: " + (str1 == str7.intern()));
+        System.out.println("str3 == str3.intern() result: " + (str3 == str3.intern()));
+        System.out.println(str1);
+        System.out.println(str7);
+    }
+}
